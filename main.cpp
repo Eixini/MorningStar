0ec565fa9ceeaed10b7d7e3c5afcd08052567cd6
Eixini <eixini@users.noreply.github.com>
2021-03-09 01:23:55 +0300

    main: replacement path to file

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ int main(int argc, char *argv[])
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Предзагрузочное окно  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     QSplashScreen *splash = new QSplashScreen; // создание объекта презагрузочного окна
-    splash->setPixmap(QPixmap("/home/eixini/Рабочий стол/Eixini/Qt/Projects/MorningStar/File_for_Project/loading_logo.png")); // установка изображения на предзагрузочное окно
+    splash->setPixmap(QPixmap(":/morningstar_resources/images/morningstar_splashscreen")); // установка изображения на предзагрузочное окно
     splash->show(); // показ предзагрузочного окна
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Главное окно   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     MorningStar window; // создание главного окна
     window.setWindowTitle("Morning Star v. Zero"); // установка заголовка на главное окно
-    window.setWindowIcon(QIcon("/home/eixini/Рабочий стол/Eixini/Qt/Projects/MorningStar/File_for_Project/ms_icon")); // установка иконки для главного окна
+    window.setWindowIcon(QIcon(":/morningstar_resources/icons/ms_icon")); // установка иконки для главного окна
     window.setFixedSize(300,250); // установка размера главного окна
 
     window.show();
